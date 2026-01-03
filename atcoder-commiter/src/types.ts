b0de13caddf4982c0352d0e09fa8bf99ffa235d9
ultsaza <torimitsu.s.aa@gmail.com>
2026-01-04 06:28:51 +0900

    feat: introduce Submission type definition for AtCoder API responses.

---
+++ atcoder-commiter/src/types.ts
@@ -0,0 +1,12 @@
+export interface Submission {
+    id: number;
+    epoch_second: number;
+    problem_id: string;
+    contest_id: string;
+    user_id: string;
+    language: string;
+    point: number;
+    length: number;
+    result: string;
+    execution_time?: number; 
+}

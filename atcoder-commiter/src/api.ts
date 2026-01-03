f0520e5a4d3ace9600c4946a50e177612e4bdc25
ultsaza <torimitsu.s.aa@gmail.com>
2026-01-04 06:49:43 +0900

    feat: add AtCoder API base URLs and initial imports for API interaction

---
+++ atcoder-commiter/src/api.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+import { Submission } from "./types";
+
+const BASE_URL = "https://kenkoooo.com/atcoder/atcoder-api/v3";
+const ATCODER_BASE_URL = "https://atcoder.jp";
+

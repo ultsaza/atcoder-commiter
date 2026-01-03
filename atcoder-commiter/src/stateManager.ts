831b59fe0e6f6e254bf12e8bae57ee9a94ac6030
ultsaza <torimitsu.s.aa@gmail.com>
2026-01-04 07:37:23 +0900

    feat: implement StateManager class for managing last timestamp state

--- atcoder-commiter/src/stateManager.ts
+++ atcoder-commiter/src/stateManager.ts
@@ -1,3 +1,15 @@
 import * as vscode from 'vscode';
 
 const STATE_KEY_LAST_TIMESTAMP = "atcoder-commiter.lastTimestamp";
+
+export class StateManager {
+    constructor(private context: vscode.ExtensionContext) {}
+
+    getLastTimestamp(): number {
+        return this.context.globalState.get<number>(STATE_KEY_LAST_TIMESTAMP, 0);
+    }
+
+    async setLastTimestamp(timestamp: number): Promise<void> {
+        await this.context.globalState.update(STATE_KEY_LAST_TIMESTAMP, timestamp);
+    }
+}

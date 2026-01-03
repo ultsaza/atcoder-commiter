9d64cf5205f0ca9503cb6e0f4c4807d8b853ffca
ultsaza <torimitsu.s.aa@gmail.com>
2026-01-04 06:58:55 +0900

    format

--- atcoder-commiter/src/api.ts
+++ atcoder-commiter/src/api.ts
@@ -5,21 +5,18 @@ const BASE_URL = "https://kenkoooo.com/atcoder/atcoder-api/v3";
 const ATCODER_BASE_URL = "https://atcoder.jp";
 
 export class ApiClient {
-    async getSubmissions(
-        userId: string,
-        fromSecond: number
-    ): Promise<Submission[]> {
-        const url = `${BASE_URL}/user/submissions`;
-        const response = await axios.get<Submission[]>(
-            url,
-            {
-                params: {
-                    user: userId,
-                    from_second: fromSecond,
-                },
-                timeout: 10000,
-            }
-        );
-        return response.data;
-    }
+  async getSubmissions(
+    userId: string,
+    fromSecond: number
+  ): Promise<Submission[]> {
+    const url = `${BASE_URL}/user/submissions`;
+    const response = await axios.get<Submission[]>(url, {
+      params: {
+        user: userId,
+        from_second: fromSecond,
+      },
+      timeout: 10000,
+    });
+    return response.data;
+  }
 }
--- atcoder-commiter/src/types.ts
+++ atcoder-commiter/src/types.ts
@@ -1,28 +1,28 @@
 export interface Submission {
-    id: number;
-    epoch_second: number;
-    problem_id: string;
-    contest_id: string;
-    user_id: string;
-    language: string;
-    point: number;
-    length: number;
-    result: string;
-    execution_time?: number; 
+  id: number;
+  epoch_second: number;
+  problem_id: string;
+  contest_id: string;
+  user_id: string;
+  language: string;
+  point: number;
+  length: number;
+  result: string;
+  execution_time?: number;
 }
 
 export const LANGUAGE_EXTENSIONS: Record<string, string> = {
-    // TODO: fill Record
-    "C++": ".cpp",
-    "Python": ".py", 
-    "Rust": ".rs",
+  // TODO: fill Record
+  "C++": ".cpp",
+  Python: ".py",
+  Rust: ".rs",
 };
 
 export function getLanguageExtension(language: string): string {
-    for (const [k, v] of Object.entries(LANGUAGE_EXTENSIONS)) {
-        if (language.startsWith(k)) {
-            return v;
-        }
+  for (const [k, v] of Object.entries(LANGUAGE_EXTENSIONS)) {
+    if (language.startsWith(k)) {
+      return v;
     }
-    return ".txt";
+  }
+  return ".txt";
 }

75ff278bbf73c0af01f82d566ce917dfa4fa0c90
ultsaza <torimitsu.s.aa@gmail.com>
2026-01-05 02:33:26 +0900

    feat: Display submission point, execution time in tooltip and add success icon to tree items.

--- atcoder-commiter/src/submissionTreeProvider.ts
+++ atcoder-commiter/src/submissionTreeProvider.ts
@@ -1,22 +1,28 @@
-import * as vscode from 'vscode';
-import { Submission } from './types';
+import * as vscode from "vscode";
+import { Submission } from "./types";
 
 export class SubmissionItem extends vscode.TreeItem {
-    constructor(
-        public readonly submission: Submission,
-        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-    ) {
-        const date = new Date(submission.epoch_second * 1000);
-        const dateStr = date.toLocaleString();
-        const label = `[${submission.problem_id}] - ${submission.result}`;
+  constructor(
+    public readonly submission: Submission,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState
+  ) {
+    const date = new Date(submission.epoch_second * 1000);
+    const dateStr = date.toLocaleString();
+    const label = `[${submission.problem_id}] - ${submission.result}`;
 
-        super(label, collapsibleState);
+    super(label, collapsibleState);
 
-        this.description = dateStr;
-        this.tooltip = `Contest: ${submission.contest_id}
+    this.description = dateStr;
+    this.tooltip = `Contest: ${submission.contest_id}
         Problem: ${submission.problem_id}
         Result: ${submission.result}
         Language: ${submission.language}
+        Point: ${submission.point}
+        Execution time: ${submission.execution_time} [ms]
         Submitted at: ${dateStr}`;
-    }
+
+    this.iconPath = new vscode.ThemeIcon(
+        "check",
+        new vscode.ThemeColor("testing.iconPassed")
+    );
 }

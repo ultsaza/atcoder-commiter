51a5c323af56b7bc66385d5db0a07a43713b9df5
ultsaza <torimitsu.s.aa@gmail.com>
2026-01-05 03:05:19 +0900

    feat: Initialize state manager and submission tree provider, removing placeholder command.

--- atcoder-commiter/src/extension.ts
+++ atcoder-commiter/src/extension.ts
@@ -1,26 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { apiClient } from "./api";
+import { StateManager } from "./stateManager";
+import { SubmissionServer } from "./submissionServer";
+import { SubmissionTreeProvider } from "./submissionTreeProvider";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "atcoder-commiter" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "atcoder-commiter" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('atcoder-commiter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from atcoder-commiter!');
-	});
-
-	context.subscriptions.push(disposable);
+  const stateManager = new StateManager(context);
+  const submissionTreeProvider = new SubmissionTreeProvider();
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
